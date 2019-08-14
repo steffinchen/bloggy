c3be989922b75374776f11557c58ef235c77b5e7
Steffi Wehrle <steffinchen@gmail.com>
2019-08-14 21:24:06 +0100

    :mag: prettier search box

--- src/common/search.js
+++ src/common/search.js
@@ -1,29 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import styled from 'styled-components';
-import { Button } from 'rebass';
+import { Button, Flex } from 'rebass';
 
 const Search = () => {
+  const [showInput, setShowInput] = useState(false);
   return (
-    <Container>
-      <SearchBox type="text" placeholder="Search..." />
-      <StyledButton primary py="5px" px="10px">
-        <FontAwesomeIcon icon={faSearch} size="xs" />
-      </StyledButton>
+    <Container pl="10px" pr="10px" py="2px;" showInput={showInput}>
+      {showInput && <SearchBox type="text" placeholder="Search..." />}
+      <FontAwesomeIcon
+        icon={faSearch}
+        size="s"
+        onClick={() => setShowInput(!showInput)}
+      />
     </Container>
   );
 };
 
-const StyledButton = styled(Button)`
-  border: 0;
-  border-radius: 0;
-`;
-
-const Container = styled.div`
-  display: flex;
-  border: 1px solid rgba(34, 36, 38, 0.15);
+const Container = styled(Flex)`
+  ${props =>
+    props.showInput
+      ? 'border: 1px solid rgba(34, 36, 38, 0.15);'
+      : 'border: none;'}
   border-radius: 5px;
+  height: 22px;
+  align-items: center;
 `;
 
 const SearchBox = styled.input`
