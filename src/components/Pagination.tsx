5d881eb7a38aef7dc7380062942155b84c7ead03
Steffi Wehrle <steffinchen@gmail.com>
2019-09-27 13:33:16 +0100

    :lipstick: style pagination

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -17,24 +17,22 @@ const navCss = css`
   }
 
   a {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell;
     background: #fff;
     color: black;
     float: left;
     padding: 8px 16px;
     text-decoration: none;
-    transition: background-color .3s;
+    transition: background-color 0.3s;
     border: 1px solid #ddd;
     margin: 0 4px;
-    box-shadow: rgba(39, 44, 49, 0.06) 8px 14px 38px, rgba(39, 44, 49, 0.03) 1px 3px 8px;
     border-radius: 6px;
     margin-bottom: 5px;
     min-width: 50px;
 
     &.active {
-      -webkit-box-shadow:inset 3px 0px 0px 0px ${darken(0.05, colors.darkgrey)};
-      -moz-box-shadow:inset 3px 0px 0px 0px ${darken(0.05, colors.darkgrey)};
-      box-shadow:inset 3px 0px 0px 0px ${darken(0.05, colors.darkgrey)};
+      -webkit-box-shadow: inset 3px 0px 0px 0px ${darken(0.05, colors.morningGlory)};
+      -moz-box-shadow: inset 3px 0px 0px 0px ${darken(0.05, colors.morningGlory)};
+      box-shadow: inset 3px 0px 0px 0px ${darken(0.05, colors.morningGlory)};
     }
 
     &:hover:not(.active) {
@@ -64,7 +62,11 @@ const Pagination: React.FunctionComponent<PaginationProps> = ({ currentPage, num
         )}
 
         {Array.from({ length: numPages }, (_, i) => (
-          <Link key={`pagination-number${i + 1}`} className={i + 1 === currentPage ? 'active' : ''} to={`/${i === 0 ? '' : i + 1}`}>
+          <Link
+            key={`pagination-number${i + 1}`}
+            className={i + 1 === currentPage ? 'active' : ''}
+            to={`/${i === 0 ? '' : i + 1}`}
+          >
             {i + 1}
           </Link>
         ))}
